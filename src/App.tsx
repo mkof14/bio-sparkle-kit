f4b3e241b2e643fa15cc3545517b42509df96783
Michael Kofman <dnainform@gmail.com>
2025-10-17 22:02:40 -0400

    fix: render only HeroLogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import HeroLogo from "@/components/HeroLogo";
+import HeroLogo from "./components/HeroLogo";
 
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <HeroLogo />
+    </main>
+  );
+}
